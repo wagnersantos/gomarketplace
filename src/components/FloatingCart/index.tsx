525fbe66675524550e1331b2ad03d6c3ab1526f5
wagnersantos <wagtvc@gmail.com>
2020-05-01 16:23:01 -0300

    added totals in  cardFloating component

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,27 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total =
+      products.length &&
+      products
+        .map(e => Number(e.price) * Number(e.quantity))
+        .reduce(
+          (accumulator: number, currentValue: number) =>
+            accumulator + currentValue,
+        );
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total =
+      products.length &&
+      products
+        .map(e => Number(e.quantity))
+        .reduce(
+          (accumulator: number, currentValue: number) =>
+            accumulator + currentValue,
+        );
+    return total;
   }, [products]);
 
   return (
